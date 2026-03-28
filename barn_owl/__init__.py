2bf82ea8fb16eb7889959b997aa2179facaa13fd
aaronthmetic <aaronhui88@gmail.com>
2026-03-27 22:39:53 -0400

    fixing init merge conflicts

--- barn_owl/__init__.py
+++ barn_owl/__init__.py
@@ -1,9 +1,11 @@
 """Public package interface for barn_owl."""
 
 from .wisdom import owl_wisdom
+from .drawing import owl_drawing
 from .latin import to_owl_latin
 
 __all__ = [
     "owl_wisdom",
-    "to_owl_latin"
+    "owl_drawing",
+    "to_owl_latin",
 ]
